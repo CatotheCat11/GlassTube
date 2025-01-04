4954f0574c45b70773c08ccc4cb2b1726b9ee0cb
Cato the Cat <syedameen.alifamily@gmail.com>
2025-01-04 19:42:10 +0300

    Initial commit

---
+++ app/src/main/java/com/cato/glasstube/MainActivity.java
@@ -0,0 +1,265 @@
+package com.cato.glasstube;
+
+import static android.content.ContentValues.TAG;
+
+import android.app.Activity;
+import android.app.appsearch.SearchResult;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.google.android.glass.media.Sounds;
+import com.google.android.glass.widget.CardBuilder;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
+
+import org.schabi.newpipe.extractor.Image;
+import org.schabi.newpipe.extractor.InfoItem;
+import org.schabi.newpipe.extractor.NewPipe;
+import org.schabi.newpipe.extractor.ServiceList;
+import org.schabi.newpipe.extractor.StreamingService;
+import org.schabi.newpipe.extractor.exceptions.ExtractionException;
+import org.schabi.newpipe.extractor.search.SearchInfo;
+import org.schabi.newpipe.extractor.stream.StreamInfoItem;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainActivity extends Activity {
+    private List<CardBuilder> mCards;
+    private CardScrollView mCardScrollView;
+    private ExampleCardScrollAdapter mAdapter;
+    private List<String> videoResults = new ArrayList<String>();
+    private static final int SPEECH_REQUEST = 0;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCards = new ArrayList<CardBuilder>();
+        mCards.add(new CardBuilder(this, CardBuilder.Layout.MENU)
+                .setText("Loading..."));
+        mCardScrollView = new CardScrollView(this);
+        mAdapter = new ExampleCardScrollAdapter();
+        mCardScrollView.setAdapter(mAdapter);
+        mCardScrollView.activate();
+        setupClickListener();
+        setContentView(mCardScrollView);
+        if (getIntent().getExtras() != null) {
+            ArrayList<String> voiceResults = getIntent().getExtras()
+                    .getStringArrayList(RecognizerIntent.EXTRA_RESULTS);
+            String voiceResult = voiceResults.get(0);
+            Log.d("MainActivity", "Voice result: " + voiceResult);
+            new videoSearch().execute(voiceResult);
+        } else {
+            Log.d(TAG, "No voice result");
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Search for a video");
+            startActivityForResult(intent, SPEECH_REQUEST);
+        }
+    }
+
+    private class ExampleCardScrollAdapter extends CardScrollAdapter {
+
+        @Override
+        public int getPosition(Object item) {
+            return mCards.indexOf(item);
+        }
+
+        @Override
+        public int getCount() {
+            return mCards.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mCards.get(position);
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return CardBuilder.getViewTypeCount();
+        }
+
+        @Override
+        public int getItemViewType(int position){
+            return mCards.get(position).getItemViewType();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return mCards.get(position).getView(convertView, parent);
+        }
+    }
+    private void setupClickListener() {
+        mCardScrollView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                am.playSoundEffect(Sounds.TAP);
+                Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+                intent.putExtra("url", videoResults.get(position));
+                startActivity(intent);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SPEECH_REQUEST && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            new videoSearch().execute(spokenText);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public class videoSearch extends AsyncTask<String, Void, List<InfoItem>> {
+        private static final String TAG = "VideoSearch";
+
+        @Override
+        protected List<InfoItem> doInBackground(String... query) {
+            try {
+                // Initialize NewPipe if you haven't already
+                NewPipe.init(DownloaderTestImpl.getInstance());
+
+                // Get YouTube service
+                StreamingService youtubeService = ServiceList.YouTube;
+
+                // Create search info for the query
+                SearchInfo searchInfo = SearchInfo.getInfo(youtubeService,
+                        youtubeService.getSearchQHFactory().fromQuery(query[0]));
+
+                mCards.remove(0);
+                runOnUiThread(() -> {
+                    mAdapter.notifyDataSetChanged();
+                });
+
+                // Get search results
+                return searchInfo.getRelatedItems();
+
+            } catch (Exception e) {
+                Log.e(TAG, "Error searching for videos", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<InfoItem> results) {
+            if (results != null) {
+                for (InfoItem item : results) {
+                    // Check if the item is a video
+                    if (item instanceof StreamInfoItem) {
+                        StreamInfoItem videoItem = (StreamInfoItem) item;
+
+                        String title = videoItem.getName();
+                        String url = videoItem.getUrl();
+                        videoResults.add(url);
+                        String uploaderName = videoItem.getUploaderName();
+                        long viewCount = videoItem.getViewCount();
+                        String duration = "";
+                        if (videoItem.getDuration() != -1) {
+                            duration = formatDuration(videoItem.getDuration());
+                        } else {
+                            duration = "\uD83D\uDD34 Live";
+                        }
+                        String thumbnailUrl = videoItem.getThumbnails().get(0).getUrl();
+                        String avatarUrl = videoItem.getUploaderAvatars().get(0).getUrl();
+                        CardBuilder card = new CardBuilder(MainActivity.this, CardBuilder.Layout.CAPTION)
+                                .setText(title)
+                                .setFootnote(uploaderName + " • " + viewCount + " views")
+                                .setTimestamp(duration + " seconds");
+                        mCards.add(card);
+                        Glide.with(MainActivity.this)
+                                .asBitmap()
+                                .load(thumbnailUrl)
+                                    .into(new CustomTarget<Bitmap>() { // Use CustomTarget to handle the Bitmap directly
+                                        @Override
+                                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                            card.addImage(resource);
+                                        }
+
+                                        @Override
+                                        public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                                        }
+
+                                        @Override
+                                        public void onLoadFailed(@Nullable Drawable errorDrawable) {
+                                            super.onLoadFailed(errorDrawable);
+                                            Log.e(TAG, "Image load failed");
+                                        }
+                                    });
+                        Glide.with(MainActivity.this)
+                                .asBitmap()
+                                .load(avatarUrl)
+                                .into(new CustomTarget<Bitmap>() { // Use CustomTarget to handle the Bitmap directly
+                                    @Override
+                                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                        card.setIcon(resource);
+                                    }
+
+                                    @Override
+                                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                                    }
+
+                                    @Override
+                                    public void onLoadFailed(@Nullable Drawable errorDrawable) {
+                                        super.onLoadFailed(errorDrawable);
+                                        Log.e(TAG, "Image load failed");
+                                    }
+                                });
+
+                        Log.d(TAG, "Found video: " + title + " by " + uploaderName + " at " + url);
+                    }
+                }
+                runOnUiThread(() -> {
+                    mAdapter.notifyDataSetChanged();
+                });
+            }
+        }
+    }
+    private String formatDuration(long seconds) {
+        if (seconds < 0) {
+            return "Invalid duration";
+        }
+
+        long days = seconds / (24 * 60 * 60);
+        long hours = (seconds % (24 * 60 * 60)) / (60 * 60);
+        long minutes = (seconds % (60 * 60)) / 60;
+        long remainingSeconds = seconds % 60;
+
+        StringBuilder formatted = new StringBuilder();
+
+        if (days > 0) {
+            formatted.append(days).append("d ");
+        }
+        if (hours > 0) {
+            formatted.append(hours).append("h ");
+        }
+        if (minutes > 0) {
+            formatted.append(minutes).append("m ");
+        }
+        if (remainingSeconds > 0 || formatted.length() == 0) {
+            formatted.append(remainingSeconds).append("s");
+        }
+
+        return formatted.toString().trim();
+    }
+}
---
+++ app/src/main/java/com/cato/glasstube/VideoActivity.java
@@ -0,0 +1,214 @@
+package com.cato.glasstube;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
+import com.google.android.glass.widget.Slider;
+
+import org.schabi.newpipe.extractor.NewPipe;
+import org.schabi.newpipe.extractor.ServiceList;
+import org.schabi.newpipe.extractor.StreamingService;
+import org.schabi.newpipe.extractor.exceptions.ExtractionException;
+import org.schabi.newpipe.extractor.stream.StreamInfo;
+import org.schabi.newpipe.extractor.stream.StreamType;
+import org.schabi.newpipe.extractor.stream.VideoStream;
+
+import java.io.IOException;
+import java.util.List;
+
+public class VideoActivity extends Activity {
+    private ExoPlayer player;
+    private PlayerView playerView;
+    private GestureDetector mGestureDetector;
+    private Slider mSlider;
+    private Slider.Indeterminate mIndeterminate;
+    private static final String TAG = "VideoActivity";
+    String videoUrl = "";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_view);
+        mGestureDetector = createGestureDetector(this);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
+        );
+        playerView = findViewById(R.id.playerView);
+        mSlider = Slider.from(playerView);
+        mIndeterminate = mSlider.startIndeterminate();
+        videoUrl = getIntent().getStringExtra("url");
+        if (videoUrl == null) {
+            // No URL passed as extra; check for app picker intent
+            Intent intent = getIntent();
+            if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+                Uri data = intent.getData();
+                if (data != null) {
+                    videoUrl = data.toString();
+                }
+            }
+        }
+        new FetchVideoStreamsTask().execute();
+    }
+
+    public class FetchVideoStreamsTask extends AsyncTask<Void, Void, StreamInfo> {
+
+        @Override
+        protected StreamInfo doInBackground(Void... voids) {
+            try {
+                NewPipe.init(DownloaderTestImpl.getInstance());
+                StreamingService youtubeService = ServiceList.YouTube;
+
+                // Get stream info directly instead of just the video streams
+                return StreamInfo.getInfo(youtubeService, videoUrl);
+            } catch (IOException | ExtractionException e) {
+                Log.e(TAG, "Error fetching stream info", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(StreamInfo streamInfo) {
+            if (streamInfo == null) {
+                Log.e(TAG, "Failed to fetch stream info");
+                finish();
+            }
+
+            try {
+                // Check if it's a live stream
+                if (streamInfo.getStreamType().equals(StreamType.LIVE_STREAM)) {
+                    // For live streams, use the HLS manifest URL if available
+                    String manifestUrl = streamInfo.getHlsUrl();
+                    if (manifestUrl != null && !manifestUrl.isEmpty()) {
+                        Log.d(TAG, "Playing HLS stream: " + manifestUrl);
+                        playVideo(manifestUrl, false);
+                        return;
+                    }
+                }
+
+                // Fall back to regular video streams if not live or HLS not available
+                List<VideoStream> videoStreams = streamInfo.getVideoStreams();
+                if (videoStreams != null && !videoStreams.isEmpty()) {
+                    // Log the total number of available streams
+                    Log.d(TAG, "Total available streams: " + videoStreams.size());
+
+                    // Log available streams
+                    for (VideoStream stream : videoStreams) {
+                        Log.d(TAG, String.format("Stream: %s, Resolution: %s, Format: %s",
+                                stream.getUrl(), stream.getResolution(), stream.getFormat().getName()));
+                    }
+                    playVideo(videoStreams.get(0).getUrl(), false); //TODO: How to detect if video is 360?
+                } else {
+                    Log.e(TAG, "No video streams available");
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Error processing stream info", e);
+                finish();
+            }
+        }
+    }
+
+    private void playVideo(String streamUrl, Boolean sphereView) {
+        if (sphereView) {
+            setContentView(R.layout.video_view_360);
+            mGestureDetector = createGestureDetector(this);
+            playerView = findViewById(R.id.playerView);
+        }
+
+        LoadControl loadControl = new DefaultLoadControl.Builder()
+                .setBufferDurationsMs(
+                        2000,
+                        5000,
+                        1000,
+                        1000)
+                .setPrioritizeTimeOverSizeThresholds(true)
+                .build();
+
+        player = new ExoPlayer.Builder(this)
+                .setLoadControl(loadControl)
+                .setReleaseTimeoutMs(5000)
+                // Limit video resolution to match Glass display
+                .setVideoScalingMode(C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING)
+                .build();
+
+        player.addListener(new Player.Listener() {
+            @Override
+            public void onPlaybackStateChanged(int state) {
+                if (state != ExoPlayer.STATE_BUFFERING && mIndeterminate != null) {
+                    mIndeterminate.hide();
+                    mIndeterminate = null;
+                    player.removeListener(this);
+                }
+            }
+        });
+
+        playerView.setPlayer(player);
+        MediaItem mediaItem = MediaItem.fromUri(streamUrl);
+        player.setMediaItem(mediaItem);
+        player.setWakeMode(C.WAKE_MODE_NETWORK);
+        player.prepare();
+        player.play();
+        playerView.onResume();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (player != null) {
+            player.release();
+        }
+    }
+    private GestureDetector createGestureDetector(Context context) {
+        GestureDetector gestureDetector = new GestureDetector(context);
+        //Create a base listener for generic gestures
+        gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
+            @Override
+            public boolean onGesture(Gesture gesture) {
+                playerView.showController();
+                if (gesture == Gesture.TAP) {
+                    if (player.isPlaying()) {
+                        player.pause();
+                        playerView.onPause();
+                    } else {
+                        player.play();
+                    }
+                    return true;
+                } else if (gesture == Gesture.SWIPE_RIGHT) {
+                    player.seekForward();
+                    return true;
+                } else if (gesture == Gesture.SWIPE_LEFT) {
+                    player.seekBack();
+                    return true;
+                }
+                return false;
+            }
+        });
+        return gestureDetector;
+    }
+    /*
+     * Send generic motion events to the gesture detector
+     */
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        if (mGestureDetector != null) {
+            return mGestureDetector.onMotionEvent(event);
+        }
+        return false;
+    }
+}
